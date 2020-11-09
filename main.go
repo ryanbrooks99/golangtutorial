b3300fae4cbc0ce1fc9f85081e516199db49d361
ryanbrooks99 <ryanbrooks99@gmail.com>
2020-11-09 13:55:43 -0500

    created route funcs

--- main.go
+++ main.go
@@ -25,29 +25,69 @@ type Author struct {
 
 }
 
+// Init books var as a slice Book struct
+
+var books []Book
+
 // Get all books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 // Get single book
 func getBook(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 // Create a new book
 func createBook(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 // Update book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+	if item.ID == params["id"] {
+		books = append(books[:index], books[index+:]...)
+		var book Book
+		_ = json.NewDecoder(r.Body).Decode(&book)
+		book.ID = strconv.Itoa(rand.Intn(10000000))
+		books = append(books, book)
+		json.NewEncoder(w).Encode(book)
+		return
+	}
+	}
+json.NewEncoder(w).Encode(books)
+
 }
 
 // Delete book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+	if item.ID == params["id"] {
+		books = append(books[:index], books[index+:]...)
+		break
+	}
+	}
+json.NewEncoder(w).Encode(books)
 }
 
 
@@ -55,6 +95,14 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// Init mux router
 	r := mux.NewRouter()
+
+	// Mock data @Todo implentment DB
+	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One", Author: &Author{
+		Firstname: "John", Lastname: "Doe"}})
+	
+	books = append(books, Book{ID: "2", Isbn: "847564", Title: "Book Two", Author: &Author{
+		Firstname: "Steve", Lastname: "Smith"}})
+
 
 	// Route handlers / Endpoints
 
